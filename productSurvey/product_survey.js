8586b8a1e5dbc457c3ebd2acf0ae04eaf68f1f4a
mtorbin <mltor8@gmail.com>
2025-12-31 12:54:10 -0500

    Add new functionality: user experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -16,6 +17,8 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userProductChoice2').innerHTML = productType;
+    document.getElementById('experienceFromUser').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
 }
