15870ccc0263336bc683551e649bddb46184f699
mtorbin <mltor8@gmail.com>
2026-01-25 15:20:46 -0500

    Update files

--- newsArticle/news_article.js
+++ newsArticle/news_article.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 
-var url = './health_article.json';
+var url = './news_article.json';
 
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
@@ -16,35 +16,15 @@ xhr.onload = function() {
         var title = document.createElement('h2');
         title.textContent = article.title;
 
-        var description = document.createElement('p');
-        description.textContent = article.description;
+        var newsText = document.createElement('p');
+        const articleText =article.news_text;
+        const textNode = document.createTextNode(articleText);
+        newsText.appendChild(textNode);
 
-        var waysHeader = document.createElement('h3');
-        waysHeader.textContent = 'Ways to Achieve:';
-
-        var waysList = document.createElement('ul');
-        article.ways_to_achieve.forEach(function(way) {
-            var listItem = document.createElement('li');
-            listItem.textContent = way;
-            waysList.appendChild(listItem);
-        });
-
-        var benefitsHeader = document.createElement('h3');
-        benefitsHeader.textContent = 'Benefits:';
-
-        var benefitsList = document.createElement('ul');
-        article.benefits.forEach(function(benefit) {
-            var listItem = document.createElement('li');
-            listItem.textContent = benefit;
-            benefitsList.appendChild(listItem);
-        });
+        // newsText.textContent = article.news_text;        // directly assigned to p element
 
         articleDiv.appendChild(title);
-        articleDiv.appendChild(description);
-        articleDiv.appendChild(waysHeader);
-        articleDiv.appendChild(waysList);
-        articleDiv.appendChild(benefitsHeader);
-        articleDiv.appendChild(benefitsList);
+        articleDiv.appendChild(newsText);
 
         articlesDiv.appendChild(articleDiv);
     });
